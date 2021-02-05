c37958cef0826b0ef7d6af47c7cb80a8542d1f6a
ala <ala@>
2021-02-05 17:10:45 +0800

    添加项目文件。

---
+++ LPRS/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace LPRS
+{
+    partial class zmz
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "File Path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(96, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 16);
+            this.panel1.TabIndex = 11;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 317);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image To Be Processed";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Location = new System.Drawing.Point(11, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(255, 291);
+            this.panel2.TabIndex = 14;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel3);
+            this.groupBox2.Location = new System.Drawing.Point(299, 62);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(233, 65);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Licence Plate";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Location = new System.Drawing.Point(6, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(221, 48);
+            this.panel3.TabIndex = 0;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.panel5);
+            this.groupBox3.Controls.Add(this.panel4);
+            this.groupBox3.Location = new System.Drawing.Point(299, 136);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(247, 217);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Histogram";
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(6, 186);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(235, 30);
+            this.panel5.TabIndex = 1;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint_1);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Location = new System.Drawing.Point(6, 25);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(235, 155);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint_1);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button8);
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.button6);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Location = new System.Drawing.Point(552, 16);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(84, 343);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(5, 310);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Save";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(5, 266);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "2Value";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(5, 227);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Marginal";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(5, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Noiseless";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(5, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Gray";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(6, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Equalize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(5, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Invert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // zmz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 372);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "zmz";
+            this.Text = "License Plate Recognition System";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LPRS/Form1.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sample;
+
+namespace LPRS
+{
+
+    public partial class zmz : Form
+    {
+        private String name;
+        private Bitmap m_Bitmap; //整幅图像
+        int[] gray = new int[256];
+        int[] rr = new int[256];
+        int[] gg = new int[256];
+        int[] bb = new int[256];
+        private Bitmap m_Bitmap1;
+        private Bitmap c_Bitmap; //车牌图像
+        private int maxX;
+        private int maxY;
+        private float count;
+        int flag = 0;
+        int xx = -1;
+        private bool aline = false;
+        private float[] gl = new float[256];
+        Pen pen1 = new Pen(Color.Black);
+
+        public zmz()
+        {
+            m_Bitmap = new Bitmap(2, 2);
+            InitializeComponent();
+            c_Bitmap = new Bitmap(2, 2);
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button3.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            panel3.Enabled = false;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Jpeg文件(*.jpg)|*.jpg|Bitmap文件(*.bmp)|*.bmp| 所有合适文件(*.bmp/*.jpg)|*.bmp/*.jpg";
+            openFileDialog.FilterIndex = 2;
+
+            openFileDialog.RestoreDirectory = true;
+
+            if (DialogResult.OK == openFileDialog.ShowDialog())
+
+            {
+                name = openFileDialog.FileName;
+                m_Bitmap = (Bitmap) Bitmap.FromFile(name, false);
+
+                this.panel2.AutoScroll = true;
+
+                this.panel2.AutoScrollMinSize = new Size((int) (m_Bitmap.Width), (int) m_Bitmap.Height);
+
+                this.button2.Enabled = true;
+                this.button4.Enabled = true;
+
+
+                panel2.Invalidate();
+                panel1.Invalidate();
+                panel3.Invalidate();
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (Filters.Brightness(m_Bitmap, gray, out gray, (int)count))
+                graydo();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Bitmap文件(*.bmp)|*.bmp| Jpeg文件(*.jpg)|*.jpg| 所有合适文件(*.bmp/*.jpg)|*.bmp/*.jpg";
+            saveFileDialog.FilterIndex = 1;
+
+            saveFileDialog.RestoreDirectory = true;
+
+            if (DialogResult.OK == saveFileDialog.ShowDialog())
+
+            {
+                c_Bitmap.Save(saveFileDialog.FileName);
+
+            }
+
+        }
+
+        private void graydo()
+        {
+            this.flag = 1;
+            count = m_Bitmap.Width * m_Bitmap.Height;
+            gl = new float[256];
+            for (int i = 0; i < 256; i++)
+                gl[i] = gray[i] / count * 1500;
+            pen1 = Pens.Red;
+            panel2.Invalidate();
+            panel4.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Filters.Invert(m_Bitmap, out gray))
+            {
+                panel5.Invalidate();
+                graydo();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Filters.zft(m_Bitmap, out gray, out rr, out gg, out bb);
+            this.button3.Enabled = true;
+            this.button5.Enabled = true;
+            panel5.Invalidate();
+            graydo();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Filters.GaussianFilter(m_Bitmap, out gray))
+                graydo();
+            this.button6.Enabled = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int ccount;
+            int yl, yr, xu, xd;
+            if (Filters.MarginalFilter(m_Bitmap, out gray, out ccount, 67, out xu, out xd, out yl, out yr, out maxX,
+                out maxY))
+            {
+
+                m_Bitmap1 = (Bitmap)Bitmap.FromFile(name, false);
+                Rectangle sourceRectangle = new Rectangle(yl, xu, yr - yl, xd - xu);
+                c_Bitmap = m_Bitmap1.Clone(sourceRectangle,
+                    PixelFormat.DontCare);
+                groupBox2.Text = "车牌";
+                groupBox2.Invalidate();
+
+                this.button8.Enabled = true;
+                this.button7.Enabled = true;
+                panel3.Invalidate();
+                graydo();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Filters.zft(c_Bitmap, out gray, out rr, out gg, out bb);
+            Rectangle sourceRectangle = new Rectangle(maxY, maxX, 66, 14);
+            Bitmap xc_Bitmap = c_Bitmap.Clone(sourceRectangle,
+                PixelFormat.DontCare);
+            if (Filters.TowValue(c_Bitmap, xc_Bitmap, maxY, maxY))
+            {
+                groupBox1.Text = "车牌（经二值化处理）";
+                groupBox1.Invalidate();
+                panel4.Invalidate();
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(m_Bitmap, new Rectangle(this.panel2.AutoScrollPosition.X, this.panel2.AutoScrollPosition.Y,
+                (int)(m_Bitmap.Width), (int)(m_Bitmap.Height)));
+        }
+
+        private void panel3_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Point ulCorner = new Point(0, 0);
+            g.DrawImage(c_Bitmap, 0, 0);
+           
+        }
+
+        private void panel4_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int p;
+            p = m_Bitmap.Width * m_Bitmap.Height;
+            //	this.l_pixel.Text=p.ToString();
+
+            int height = this.panel4.Height;
+            for (int j = 0; j < 256; j++)
+            {
+                if (gl[j] > height)
+                    gl[j] = height;
+                g.DrawLine(pen1, j, height, j, height - gl[j]);
+            }
+            if (aline)
+            {
+                g.DrawLine(Pens.OrangeRed, xx, 0, xx, height);
+            }
+        }
+
+        private void panel4_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            xx = e.X;
+            if (xx > 255)
+                xx = 255;
+            if (xx <= 0)
+                xx = 0;
+
+            aline = true;
+            this.panel4.Invalidate();
+        }
+
+        private void panel4_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+            if (new Rectangle(0, 0, 256, 127).Contains(p))
+            {
+                this.xx = e.X;
+            }
+            else
+            {
+                this.xx = -1;
+            }
+
+            this.panel4.Invalidate();
+        }
+
+        private void panel4_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            aline = false;
+        }
+
+        private void panel5_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            int width = this.panel5.Width;
+            int height = this.panel5.Height;
+            int j;
+            Color c;
+            Graphics g = e.Graphics;
+
+            switch (flag)
+            {
+                case 1:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(j, j, j);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(j, 0, 0);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                case 3:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(0, j, 0);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                case 4:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(0, 0, j);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                default:
+                    break;
+
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawString(name, new Font("Arial", 8), new SolidBrush(Color.Black), 0, 0);
+        }
+
+
+        private void panel4_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int p;
+            p = m_Bitmap.Width * m_Bitmap.Height;
+            //	this.l_pixel.Text=p.ToString();
+
+            int height = this.panel4.Height;
+            for (int j = 0; j < 256; j++)
+            {
+                if (gl[j] > height)
+                    gl[j] = height;
+                g.DrawLine(pen1, j, height, j, height - gl[j]);
+            }
+            if (aline)
+            {
+                g.DrawLine(Pens.OrangeRed, xx, 0, xx, height);
+            }
+        }
+
+        private void panel5_Paint_1(object sender, PaintEventArgs e)
+        {
+            int width = this.panel5.Width;
+            int height = this.panel5.Height;
+            int j;
+            Color c;
+            Graphics g = e.Graphics;
+
+            switch (flag)
+            {
+                case 1:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(j, j, j);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(j, 0, 0);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                case 3:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(0, j, 0);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                case 4:
+                {
+                    for (int i = width; i >= 0; i--)
+                    {
+                        j = i;
+                        if (j > 255) j = 255;
+                        c = Color.FromArgb(0, 0, j);
+                        Pen pen2 = new Pen(c, 1);
+                        g.DrawLine(pen2, i, 0, i, height);
+                    }
+                    break;
+                }
+                default:
+                    break;
+
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
